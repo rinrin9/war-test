a6b3dedc457ebe5aea2e611a7770cb86fff90f0a
rinrin <r.chaaaaaan@gmail.com>
2022-10-25 13:22:50 +0900

    15-課題2

---
+++ src/com/example/Sample.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Sample {
+
+    public static String getColor(int rgb) {
+        switch (rgb) {
+        case 1:
+            return "赤";
+        case 2:
+            // 本来は「緑」
+            return "緑";
+        case 3:
+            return "青";
+        }
+        throw new IllegalArgumentException();
+    }
+}
